b5d9c208b8a52457ef526eb2fe8374e7e28dd878
AndreaArellanoBello <71836286+AndreaArellanoBello@users.noreply.github.com>
2021-03-04 16:36:11 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      '/pwa-examples/a2hs/',
-      '/pwa-examples/a2hs/index.html',
-      '/pwa-examples/a2hs/index.js',
-      '/pwa-examples/a2hs/style.css',
-      '/pwa-examples/a2hs/images/fox1.jpg',
-      '/pwa-examples/a2hs/images/fox2.jpg',
-      '/pwa-examples/a2hs/images/fox3.jpg',
-      '/pwa-examples/a2hs/images/fox4.jpg',
+      '/Practica/',
+      '/Practica/index.html',
+      '/Practica/index.js',
+      '/Practica/style.css',
+      '/Practica/images/image1.jpg',
+      '/Practica/images/image2.jpg',
+      '/Practica/images/image3.jpg',
+      '/Practica/images/image4.jpg',
     ])),
   );
 });
